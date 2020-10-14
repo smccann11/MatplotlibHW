dc593414159b1d40c8b8d5ded1a26d9194692629
smcann11 <70291882+smcann11@users.noreply.github.com>
2020-10-13 20:50:16 -0700

    Create chart2.py

---
+++ chart2.py
@@ -0,0 +1,18 @@
+import matplotlib
+import pandas as pd
+
+df_raw = pd.read_csv("https://github.com/selva86/datasets/raw/master/mpg_ggplot2.csv")
+df = df_raw[['cty', 'manufacturer']].groupby('manufacturer').apply(lambda x: x.mean())
+df.sort_values('cty', inplace=True)
+df.reset_index(inplace=True)
+
+fig, ax = plt.subplots(figsize=(16,10), dpi= 80)
+ax.hlines(y=df.index, xmin=11, xmax=26, color='gray', alpha=0.7, linewidth=1, linestyles='dashdot')
+ax.scatter(y=df.index, x=df.cty, s=75, color='firebrick', alpha=0.7)
+
+ax.set_title('Dot Plot for Highway Mileage', fontdict={'size':22})
+ax.set_xlabel('Miles Per Gallon')
+ax.set_yticks(df.index)
+ax.set_yticklabels(df.manufacturer.str.title(), fontdict={'horizontalalignment': 'right'})
+ax.set_xlim(10, 27)
+plt.show()
