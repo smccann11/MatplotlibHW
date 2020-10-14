e139be3d5760d12583e60b6170b47b2c2eeaf2f9
smcann11 <70291882+smcann11@users.noreply.github.com>
2020-10-13 21:03:00 -0700

    Update chart2.py

--- chart2.py
+++ chart2.py
@@ -1,5 +1,25 @@
-import matplotlib
+import numpy as np
 import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings; warnings.filterwarnings(action='once')
+
+large = 22; med = 16; small = 12
+params = {'axes.titlesize': large,
+          'legend.fontsize': med,
+          'figure.figsize': (16, 10),
+          'axes.labelsize': med,
+          'axes.titlesize': med,
+          'xtick.labelsize': med,
+          'ytick.labelsize': med,
+          'figure.titlesize': large}
+plt.rcParams.update(params)
+plt.style.use('seaborn-whitegrid')
+sns.set_style("white")
+
+print(mpl.__version__)  #> 3.0.0
+print(sns.__version__)  #> 0.9.0
 
 df_raw = pd.read_csv("https://github.com/selva86/datasets/raw/master/mpg_ggplot2.csv")
 df = df_raw[['cty', 'manufacturer']].groupby('manufacturer').apply(lambda x: x.mean())
@@ -8,7 +28,7 @@ df.reset_index(inplace=True)
 
 fig, ax = plt.subplots(figsize=(16,10), dpi= 80)
 ax.hlines(y=df.index, xmin=11, xmax=26, color='gray', alpha=0.7, linewidth=1, linestyles='dashdot')
-ax.scatter(y=df.index, x=df.cty, s=75, color='firebrick', alpha=0.7)
+ax.scatter(y=df.index, x=df.cty, s=75, color='red', alpha=0.7)
 
 ax.set_title('Dot Plot for Highway Mileage', fontdict={'size':22})
 ax.set_xlabel('Miles Per Gallon')
